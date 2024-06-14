2b7220e87a59fc9099785bf5841600f551a27621
upsarpanch <jatekomal@gmail.com>
2024-06-14 07:16:48 +0530

    first commit

---
+++ src/main/java/com/flipkart/flipkart_backend_service/FlipkartBackendServiceApplication.java
@@ -0,0 +1,17 @@
+package com.flipkart.flipkart_backend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartBackendServiceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartBackendServiceApplication.class, args);
+
+	}
+	
+	
+
+}
---
+++ src/test/java/com/flipkart/flipkart_backend_service/FlipkartBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipkart.flipkart_backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
